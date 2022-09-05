32c285c1594068604ef9c95ce23cb717985e760d
umarinfotechgroup <81422941+umarinfotechgroup@users.noreply.github.com>
2022-09-05 09:56:50 +0500

    content projection implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {RouterModule} from "@angular/router";
-import {LayoutModule} from "./component/classes/layout/layout.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CommonModule} from "@angular/common";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {ToastrModule} from "ngx-toastr";
 import {AppUtility} from "./util/app.utility";
-import {AppService} from "./service/app.service";
 
 @NgModule({
     declarations: [
@@ -26,7 +24,6 @@ import {AppService} from "./service/app.service";
         FormsModule,
         HttpClientModule,
         BrowserAnimationsModule,
-        LayoutModule,
         ToastrModule.forRoot({
             timeOut: 5000,
             closeButton: true
