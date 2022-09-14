79487dd5ff7878121ef79adc2b56ce00a4ba682e
umarinfotechgroup <81422941+umarinfotechgroup@users.noreply.github.com>
2022-09-14 14:19:17 +0500

    issues fixed

--- src/app/component/setting/um/role/add-role/add-role.component.ts
+++ src/app/component/setting/um/role/add-role/add-role.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-add-role',
-  templateUrl: './add-role.component.html',
-  styleUrls: ['./add-role.component.less']
+    selector: 'app-add-role',
+    templateUrl: './add-role.component.html',
+    styleUrls: ['./add-role.component.less']
 })
 export class AddRoleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    
+    constructor() {
+    }
+    
+    ngOnInit(): void {
+    }
+    
 }
