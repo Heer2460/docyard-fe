464da5d09b134b8a84922ce0074ef9305a7ddee9
Haris.Ahmed <haris.ahmed@infotechgroup.com>
2022-10-10 13:43:01 +0500

    bu id 7 done

--- src/app/component/setting/um/user/add-user/add-user.component.ts
+++ src/app/component/setting/um/user/add-user/add-user.component.ts
@@ -98,7 +98,7 @@ export class AddUserComponent implements OnInit, OnDestroy {
     buildForms() {
         this.addUserForm = this.fb.group({
             username: [null, [Validators.required, Validators.maxLength(35)]],
-            name: [null, [Validators.required, Validators.maxLength(70)]],
+            name: [null, [Validators.required, Validators.maxLength(32), Validators.pattern(/^[a-zA-Z0-9]*$/)]],
             email: [null, [Validators.required, Validators.email, Validators.maxLength(50)]],
             phoneNumber: [null, [Validators.maxLength(17)]],
             mobileNumber: [null, [Validators.maxLength(17)]],
