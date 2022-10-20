5aa568f968e97bb0018e11ea720aa6cc60e33756
Irfan Nasim <irfan.nasim@infotechgroup.com>
2022-10-20 18:07:28 +0500

    multiplecanges\

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -64,6 +64,7 @@ export class HomeComponent implements OnInit {
     ngOnInit(): void {
         this.getRecentDocument();
         this.buildForms();
+        this.getDashboardStats();
     }
 
     buildForms() {
